221e2ad84f0a75890abf4f439d158f0c971d767d
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-10-31 13:57:08 +0000

    Added trigger for saving old price but doesnt work in PHPmyAdmin

---
+++ SQL/Triggers/SaveOldPrice.sql
@@ -0,0 +1,12 @@
+CREATE TRIGGER SaveOldProdPrice 
+AFTER UPDATE
+ON pub_product
+FOR EACH ROW
+BEGIN
+INSERT INTO pub_product_price_audit(ProdID, DateChanged, NewPrice, OldPrice, Quantity) 
+    SELECT pub_product.ProdID,GETDATE(),new.Price,old.Price,pub_product.Qty
+    FROM pub_product
+    JOIN inserted AS new ON new.ProdID = pub_product.ProdID
+    JOIN deleted AS old ON old.ProdID = pub_product.ProdID
+    WHERE new.Price <> old.Price;
+END;
