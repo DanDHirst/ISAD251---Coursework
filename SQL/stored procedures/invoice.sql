654c366d88abd1f3378079016d75a9ab5e34a149
Dan Hirst <39267897+DanDHirst@users.noreply.github.com>
2019-10-31 15:26:37 +0000

    Syntax error with end and begin statment

--- SQL/stored procedures/invoice.sql
+++ SQL/stored procedures/invoice.sql
@@ -1,10 +1,7 @@
 CREATE PROCEDURE Invoice (IN OrderNumber INT)
-BEGIN
     SELECT pub_order.OrderDate, pub_order_detail.ProdID, pub_order_detail.Qty, 
     pub_product.ProdName, pub_product.Price, (pub_order_detail.Qty * pub_product.Price) as SubTotal
     FROM pub_order, pub_order_detail, pub_product
     WHERE pub_order.OrderID = OrderNumber
     AND pub_order_detail.OrderID = pub_order.OrderID
     AND pub_order_detail.ProdID = pub_product.ProdID
-
-END;
