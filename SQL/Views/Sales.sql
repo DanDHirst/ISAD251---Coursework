ebc07af58fcb0dfb1e19890778b3b63ba754c1ae
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-01 10:51:21 +0000

    Modified the sales view so it ordered by orderid

--- SQL/Views/Sales.sql
+++ SQL/Views/Sales.sql
@@ -1,6 +1,7 @@
 CREATE VIEW Sales AS
-SELECT sum(pub_order_detail.Qty * pub_order_detail.Price) AS TotalCost,
- pub_order.OrderID, pub_order.Email
+SELECT pub_order.OrderID, pub_order.Email,
+ sum(pub_order_detail.Qty * pub_order_detail.Price) AS TotalCost
  FROM pub_order,pub_order_detail
  WHERE pub_order.OrderID = pub_order_detail.OrderID
- GROUP BY pub_order.OrderID, pub_order.Email;
+ GROUP BY pub_order.OrderID, pub_order.Email
+ ORDER BY pub_order.OrderID;
