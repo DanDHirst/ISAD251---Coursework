671e903812948ebd24bac2d891ee5abcd1eb89b0
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-01 11:15:12 +0000

    Bug fix on prodecure Invoice total value was calculating the value from the products table, should have been calcualting from the order_details table

--- SQL/stored procedures/invoice.sql
+++ SQL/stored procedures/invoice.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE Invoice (OrderNumber INT)
     SELECT pub_order.OrderDate, pub_order_detail.ProdID, pub_order_detail.Qty, 
-    pub_product.ProdName, pub_product.Price, (pub_order_detail.Qty * pub_product.Price) as SubTotal
+    pub_product.ProdName, pub_order_detail.Price, (pub_order_detail.Qty * pub_order_detail.Price) as SubTotal
     FROM pub_order, pub_order_detail, pub_product
     WHERE pub_order.OrderID = OrderNumber
     AND pub_order_detail.OrderID = pub_order.OrderID
