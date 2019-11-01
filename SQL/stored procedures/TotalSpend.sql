afaa7d7200ce3cbf7b493d8b7e04f0e3cc850726
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-01 11:59:46 +0000

    added new procedure total spend for a customer

--- SQL/stored procedures/TotalSpend.sql
+++ SQL/stored procedures/TotalSpend.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE TotalSpend AS
-SELECT pub_order.Email, SUM(pub_order_detail.Qty * pub_order_detail.Price) as SubTotal
-    FROM pub_order, pub_order_detail, pub_product
+SELECT pub_order.Email, sum(pub_order_detail.Qty * pub_order_detail.Price) as SubTotal
+    FROM pub_order, pub_order_detail
     WHERE pub_order.OrderID = pub_order_detail.OrderID
     AND pub_order.Email = CusEmail
     GROUP BY pub_order.Email
