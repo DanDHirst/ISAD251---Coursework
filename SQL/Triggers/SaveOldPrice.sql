e4f0d993eb5c532e06e442c674bb7f3b1d12f3a4
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-07 11:59:52 +0000

    bug fix in where statment, the trigger saved all of the products not just the one that chnaged

--- SQL/Triggers/SaveOldPrice.sql
+++ SQL/Triggers/SaveOldPrice.sql
@@ -1,24 +1,7 @@
-CREATE TRIGGER `saveOldPrice`
-AFTER
-UPDATE
-  ON `pub_product` FOR EACH ROW
-INSERT INTO
-  pub_product_price_audit(
-    ProdID,
-    DateChanged,
-    NewPrice,
-    OldPrice,
-    Quantity
-  )
-SELECT
-  pub_product.ProdID,
-  GETDATE(),
-  new.Price,
-  old.Price,
-  pub_product.Qty
-FROM
-  pub_product
-  JOIN inserted AS new ON new.ProdID = pub_product.ProdID
-  JOIN deleted AS old ON old.ProdID = pub_product.ProdID
-WHERE
-  new.Price <> old.Price
+CREATE TRIGGER `saveOldPrice` AFTER UPDATE ON `pub_product`
+ FOR EACH ROW 
+ INSERT INTO pub_product_price_audit(ProdID, DateChanged, NewPrice, OldPrice, Quantity) 
+    SELECT pub_product.ProdID,NOW(),NEW.Price,OLD.Price,pub_product.Qty
+    FROM pub_product
+    WHERE NEW.Price <> OLD.Price
+    AND pub_product.ProdID = NEW.ProdID
