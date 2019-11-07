7289b32d7e99a24bc83c388c7329cd8ed51a1517
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-07 11:38:24 +0000

    added the trigger save old price

--- SQL/Triggers/SaveOldPrice.sql
+++ SQL/Triggers/SaveOldPrice.sql
@@ -1,12 +1,24 @@
-CREATE TRIGGER SaveOldProdPrice 
-AFTER UPDATE
-ON pub_product
-FOR EACH ROW
-BEGIN
-INSERT INTO pub_product_price_audit(ProdID, DateChanged, NewPrice, OldPrice, Quantity) 
-    SELECT pub_product.ProdID,GETDATE(),new.Price,old.Price,pub_product.Qty
-    FROM pub_product
-    JOIN inserted AS new ON new.ProdID = pub_product.ProdID
-    JOIN deleted AS old ON old.ProdID = pub_product.ProdID
-    WHERE new.Price <> old.Price;
-END;
+CREATE TRIGGER `saveOldPrice`
+AFTER
+UPDATE
+  ON `pub_product` FOR EACH ROW
+INSERT INTO
+  pub_product_price_audit(
+    ProdID,
+    DateChanged,
+    NewPrice,
+    OldPrice,
+    Quantity
+  )
+SELECT
+  pub_product.ProdID,
+  GETDATE(),
+  new.Price,
+  old.Price,
+  pub_product.Qty
+FROM
+  pub_product
+  JOIN inserted AS new ON new.ProdID = pub_product.ProdID
+  JOIN deleted AS old ON old.ProdID = pub_product.ProdID
+WHERE
+  new.Price <> old.Price
