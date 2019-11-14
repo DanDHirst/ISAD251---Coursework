7503268b45c077241a82c5f672ef53cd285b94ce
Dan <Danhirst123@gmail.com>
2019-11-14 17:53:58 +0000

    added models for the views get snack and drink

---
+++ pub_site/app/GetSnack.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GetSnack extends Model
+{
+    //
+    protected $table = 'GetSnacks';
+    protected $primaryKey = 'ProdID';
+}
