385fd8aadac848e5279811a0c353b22c1fdaefa7
(s) Daniel Hirst <daniel.hirst@students.plymouth.ac.uk>
2019-11-20 15:20:20 +0000

    added stored procedures for deleting an order and deleting order details

--- SQL/stored procedures/getOrder.sql
+++ SQL/stored procedures/getOrder.sql
@@ -1,4 +1,8 @@
+DELIMITER $$
+CREATE DEFINER=`ISAD251_DHirst`@`%` PROCEDURE `getOrder`(IN `newOrderID` INT, IN `newEmail` VARCHAR(50))
+    NO SQL
 SELECT OrderID, OrderDate, Completed, Comments, TableNumber, Email
     FROM pub_order
     WHERE newOrderID = OrderID
-    OR Email = newEmail
+    OR Email = newEmail$$
+DELIMITER ;
--- SQL/stored procedures/getOrderDetails.sql
+++ SQL/stored procedures/getOrderDetails.sql
@@ -1,3 +1,7 @@
+DELIMITER $$
+CREATE DEFINER=`ISAD251_DHirst`@`%` PROCEDURE `getOrderDetails`(IN `newOrderID` INT)
+    NO SQL
 SELECT OrderID, OrderDetailsID, ProdID, Qty, Price
 FROM pub_order_detail
-WHERE newOrderID = OrderID
+WHERE newOrderID = OrderID$$
+DELIMITER ;
