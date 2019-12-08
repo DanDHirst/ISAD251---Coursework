fa72c23146e174d4d939755a7225b91cadbb999d
Dan Hirst <danhirst123@gmail.com>
2019-12-08 16:23:33 +0000

    Bug Fix: The button on home page was unable to redirect to the order page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/PubImages/PubBottles.jpg&quot;);color:rgba(9, 162, 255, 0.85);">
         <div class="text">
             <h2>Pub Open since 1904</h2>
-            <p>Hope you come to visit us soon!</p><button class="btn btn-outline-light btn-lg" type="button">Order Here</button></div>
+            <p>Hope you come to visit us soon!</p> <a class="nav-link" href="order"><button class="btn btn-outline-light btn-lg" type="button">Order Here</button></a></div>
     </section>
     <section class="clean-block clean-info dark">
         <div class="container">
